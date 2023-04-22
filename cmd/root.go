54035a460905a18ef9a0863558a8cfe169137ebb
enuesaa <nsrciog@gmail.com>
2023-04-23 03:26:49 +0900

    feat: use go-diff

--- cmd/root.go
+++ cmd/root.go
@@ -2,14 +2,30 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"github.com/spf13/cobra"
+	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "difii",
+	Args: cobra.MinimumNArgs(1),
 	Run: rootCmdHandler,
 }
 
 func rootCmdHandler(cmd *cobra.Command, args []string)  {
-	fmt.Println("aa")
+	dirname := args[0]
+	files, err := os.ReadDir(dirname)
+	if err != nil {
+		fmt.Printf("%+v", err)
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		fmt.Printf("%s", file)
+	}
+
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain("あ", "あか", false)
+	fmt.Println(dmp.DiffPrettyText(diffs))
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/spf13/cobra v1.5.0
 require (
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/sergi/go-diff v1.3.1 // indirect
 	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
 )
 
