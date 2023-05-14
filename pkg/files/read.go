154e3a1c8a07719cc4713ff158478c91c798a841
enuesaa <nsrciog@gmail.com>
2023-05-15 02:44:45 +0900

    feat: 簡易的な diff を行えるようにした

--- pkg/files/read.go
+++ pkg/files/read.go
@@ -30,3 +30,39 @@ func ReadStream(dir string, filename string) {
 		fmt.Println(scanner.Text())
 	}
 }
+
+
+func ReadStreamWithDiff(sourceDir string, destinationDir string, filename string) {
+	source, err := os.Open(sourceDir + "/" + filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer source.Close()
+
+	destination, err := os.Open(destinationDir + "/" + filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer destination.Close()
+
+	sourceScanner := bufio.NewScanner(source)
+	destinationScanner := bufio.NewScanner(destination)
+
+	// todo hunked
+	// todo when destination holds longer bytes than source
+	for sourceScanner.Scan() {
+		if destinationScanner.Scan() {
+			sourceText := sourceScanner.Text()
+			destinationText := destinationScanner.Text()
+			if sourceText != destinationText {
+				fmt.Printf("+ %s\n", sourceText)
+				fmt.Printf("- %s\n", destinationText)
+			}
+		} else {
+			sourceText := sourceScanner.Text()
+			fmt.Printf("+ %s\n", sourceText)
+		}
+	}
+}
