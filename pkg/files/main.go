edff0a165a80c1630e0fa4db697aaf8110e2daa5
enuesaa <nsrciog@gmail.com>
2023-05-20 23:57:13 +0900

    chore: remove unused functions

--- pkg/files/main.go
+++ pkg/files/main.go
@@ -6,23 +6,6 @@ import (
 	"strings"
 )
 
-func ListFiles(dir string) []string {
-	files, _ := os.ReadDir(dir)
-	filenames := make([]string, 0)
-	for _, file := range files {
-		if file.Name() == ".git" {
-			continue
-		}
-		if file.IsDir() {
-			filenames = append(filenames, file.Name()+"/")
-		} else {
-			filenames = append(filenames, file.Name())
-		}
-	}
-
-	return filenames
-}
-
 func ListFilesRecursively(dir string) []string {
 	filenames := make([]string, 0)
 	filepath.Walk(dir, func(path string, file os.FileInfo, err error) error {
--- pkg/files/read.go
+++ pkg/files/read.go
@@ -7,30 +7,6 @@ import (
 	"fmt"
 )
 
-func Read(dir string, filename string) string {
-	bytes, err := os.ReadFile(dir + "/" + filename)
-	if err != nil {
-		return ""
-	}
-	return string(bytes)
-}
-
-func ReadStream(dir string, filename string) {
-	// see https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
-	// see https://zenn.dev/hsaki/books/golang-io-package/viewer/bufio
-	file, err := os.Open(dir + "/" + filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-}
-
 func ReadStreamWithDiff(sourceDir string, destinationDir string, filename string) {
 	source, err := os.Open(sourceDir + "/" + filename)
 	if err != nil {
