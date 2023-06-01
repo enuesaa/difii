036e76bb67fc4bf67042b36494f6d7e7b07a8a20
enuesaa <nsrciog@gmail.com>
2023-06-02 00:41:29 +0900

    feat: summary に sourcePath と destPath を表示する

--- pkg/cli/summary.go
+++ pkg/cli/summary.go
@@ -10,21 +10,20 @@ import (
 )
 
 func Summary(input CliInput) {
-	fmt.Printf("Source dir: %s \n", input.SourceDir)
-	fmt.Printf("Destination dir: %s \n", input.DestinationDir)
-	fmt.Printf("\n")
-	fmt.Println("Summary")
+	fmt.Printf("\nSummary..\n")
 	sourcefiles := files.ListFilesRecursively(input.SourceDir)
 
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"diffs", "filename"})
+	table.SetHeader([]string{"filename", "diffs", "source", "dest"})
 
 	for _, filename := range sourcefiles {
-		source := files.ReadStream(input.SourceDir + "/" + filename)
-		dest := files.ReadStream(input.DestinationDir + "/" + filename)
+		sourcePath := input.SourceDir + "/" + filename
+		destPath := input.DestinationDir + "/" + filename
+		source := files.ReadStream(sourcePath)
+		dest := files.ReadStream(destPath)
 		analyzer := diff.NewAnalyzer(source, dest)
 		diffs := analyzer.Analyze()
-		table.Append([]string{diffs.Summary(), filename})
+		table.Append([]string{filename, diffs.Summary(), sourcePath, destPath})
 	}
 	table.Render()
 }
