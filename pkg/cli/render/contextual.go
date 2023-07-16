14781519f3b703c47098f35dfce76940311968c9
enuesaa <nsrciog@gmail.com>
2023-07-16 19:03:16 +0900

    feat: 若干表示にムラがあるがハンクの前後の行を表示できるようにした

--- pkg/cli/render/contextual.go
+++ pkg/cli/render/contextual.go
@@ -2,8 +2,8 @@ package render
 
 import (
 	"fmt"
-	"bufio"
 	"io"
+	"strings"
 
 	"github.com/enuesaa/difii/pkg/diff"
 	"github.com/fatih/color"
@@ -11,52 +11,42 @@ import (
 
 type ContextualRenderer struct {
 	diffs diff.Diffs
-	dest  bufio.Scanner
-	last int
+	lines []string
 }
 
 func NewContextualRenderer(diffs diff.Diffs, dest io.Reader) *ContextualRenderer {
+	raw, _ := io.ReadAll(dest)
+	lines := strings.Split(string(raw), "\n")
+	
 	return &ContextualRenderer{
 		diffs: diffs,
-		dest: *bufio.NewScanner(dest),
-		last: 0,
+		lines: lines,
 	}
 }
 
 func (ren *ContextualRenderer) Render() {
 	for _, hunk := range ren.diffs.ListHunks() {
-		for _, item := range hunk.ListItems() {
-			ren.renderSameLine(item.Line())
+		var last int
+		for i, item := range hunk.ListItems() {
+			last = item.Line()
+			if i == 0 {
+				fmt.Println(ren.getLine(last - 2))
+			}
 			if item.Added() {
 				fmt.Println(color.GreenString("+ " + item.Text()))
 			} else {
 				fmt.Println(color.RedString("- " + item.Text()))
 			}
 		}
+		fmt.Println(ren.getLine(last))
 	}
 	// ren.renderSameLine(-1)
 	fmt.Printf("\n")
 }
 
-// Pattern:
-//   last:0, next:3   .. 一番初めのhunk
-//   last:6, next:10  .. 次のhunk
-//   last:11, next:-1 .. 最後
-func (ren *ContextualRenderer) renderSameLine(current int) {
-	if ren.last > current {
-		return;
+func (ren *ContextualRenderer) getLine(line int) string {
+	if len(ren.lines) > line {
+		return ren.lines[line]
 	}
-	// -1 
-	if ren.last == current - 1 {
-		ren.dest.Scan()
-		ren.last += 1
-		return;
-	}
-	for i := ren.last; i < current - 1; i++ {
-		ren.dest.Scan()
-		if i >= current - 2 {
-			fmt.Println(ren.dest.Text())
-		}
-	}
-	ren.last = current
+	return ""
 }
