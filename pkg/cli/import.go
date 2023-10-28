47718bbd615cb9d30d019e06bca1b55e21c00bbe
enuesaa <nsrciog@gmail.com>
2023-10-28 17:10:23 +0900

    feat: proceed to develop import command

--- pkg/cli/import.go
+++ pkg/cli/import.go
@@ -2,6 +2,10 @@ package cli
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"slices"
+	"strings"
 
 	"github.com/enuesaa/difii/pkg/diff"
 	"github.com/enuesaa/difii/pkg/repo"
@@ -11,7 +15,7 @@ import (
 type ImportService struct{}
 
 func (srv *ImportService) Confirm(fsio repo.FsioInterface) bool {
-	return fsio.Confirm("Would you like to import diffs?")
+	return fsio.Confirm("Would you like to import diffs? Warning: NOT STABLE")
 }
 
 func (srv *ImportService) Render(fsio repo.FsioInterface, input CliInput) {
@@ -28,11 +32,11 @@ func (srv *ImportService) Render(fsio repo.FsioInterface, input CliInput) {
 		analyzer := diff.NewAnalyzer(comparedir, workdir)
 		diffs := analyzer.Analyze()
 
-		srv.renderHunks(fsio, filename, *diffs)
+		srv.renderHunks(fsio, filename, *diffs, input)
 	}
 }
 
-func (srv *ImportService) renderHunks(fsio repo.FsioInterface, filename string, diffs diff.Diffs) {
+func (srv *ImportService) renderHunks(fsio repo.FsioInterface, filename string, diffs diff.Diffs, input CliInput) {
 	for _, hunk := range diffs.ListHunks() {
 		for _, item := range hunk.ListItems() {
 			line := fmt.Sprint(item.Line())
@@ -43,11 +47,39 @@ func (srv *ImportService) renderHunks(fsio repo.FsioInterface, filename string, 
 			}
 		}
 		if fsio.Confirm("Would you like to import this hunk?") {
-			srv.importHunk(fsio, filename, hunk)
+			srv.importHunk(fsio, filename, hunk, input)
 		}
 	}
 }
 
-func (srv *ImportService) importHunk(fsio repo.FsioInterface, filename string, hunk diff.Hunk) {
-	fsio.Printf("%+v\n", hunk)
+func (srv *ImportService) importHunk(fsio repo.FsioInterface, filename string, hunk diff.Hunk, input CliInput) {
+	path := fmt.Sprintf("%s/%s", input.WorkDir, filename)
+
+	// create file if not exist
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		f, _ := os.Create(path)
+		defer f.Close()
+	}
+
+	f, _ := os.Open(path)
+	defer f.Close()
+	contentbyte, _ := io.ReadAll(f)
+	lines := strings.Split(string(contentbyte), "\n")
+
+	for _, item := range hunk.ListItems() {
+		if item.Added() {
+			lines = slices.Insert(lines, item.Line(), item.Text())
+		} else {
+			lines = slices.Delete(lines, item.Line() - 1, item.Line())
+		}
+	}
+
+	if len(lines) == 0 {
+		os.Remove(path)
+	} else {
+		content := strings.Join(lines, "\n")
+		f, _ := os.Create(path)
+		defer f.Close()
+		f.Write([]byte(content))
+	}
 }
